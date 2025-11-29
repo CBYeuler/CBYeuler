e6bdd4757651dac1da796ecd9c2806111c77ec4b
CBYeuler <sherbeb153@gmail.com>
2025-11-29 17:24:51 +0300

    Add metrics display in metrics.go
    Initial implementation of metrics display in Go.

---
+++ scripts/metrics.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// Placeholder metrics - sonra GitHub API entegre ederiz
+	fmt.Println("Lines of Go Code: 24,312")
+	fmt.Println("Lines of Python Code: 9,408")
+	fmt.Println("Repositories: 18")
+	fmt.Println("Commits This Month: 128")
+	fmt.Println("Automated Workflows Triggered: 42")
+}
