bcc880989927491b5fe69558be0e5b5655ee2837
CBYeuler <sherbeb153@gmail.com>
2025-11-29 17:26:20 +0300

    Add heatmap script to display random values

---
+++ scripts/heatmap.py
@@ -0,0 +1,8 @@
+import os
+import random
+
+print("Go       " + "█" * random.randint(10,20))
+print("Python   " + "█" * random.randint(5,15))
+print("Bash     " + "█" * random.randint(1,8))
+print("TS       " + "█" * random.randint(2,12))
+print("Other    " + "█" * random.randint(1,6))
